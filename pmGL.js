e42b45b5a7317d0016fc5503617bbfdb1e704f2a
smartguy1196 <smartguy1196@gmail.com>
2017-10-29 02:40:34 -0700

    Flow improvements, placeholders, and image extraction
    - loggerBox name removed for now, placeholder set in place
    - placeholder will also hold functions that's design is still indecisive
    - added placeholders for console warnings, and URI handling
    - added callback control to staging functions
    -- This is to adapt for the image extraction process - image data for plotting is not available until image load event, hence a callback function is appropriate.
    -- in the staging *method*, a default callback is set - this callback creates a plot object immediately once the image data had been extracted
    -- in the staging function, this callback is used optionally, and is called when the image is loaded
    -- staging function still returns source object, and now, so does the staging method
    --- this change in staging method behavior, as said, is due to the image extraction process, which means that `this.plot` will not be immediately available
    - added plot getter function to accomodate for image extraction
    - added image extraction process to staging function
    -- staging function still needs work for modifying the resource string

--- pmGL.js
+++ pmGL.js
@@ -1,9 +1,11 @@
 // log handling:
 
-var loggerBox = {  //These functions are namespaced here in case they will be changed in the future
+var placeHolders = {  //These functions are namespaced here in case they will be changed in the future
 	
 	"log" : ( log ) => { console.log( log ) },
-	"error" : ( error ) => { console.log( error ) }
+	"error" : ( error ) => { console.log( error ) },
+	"warn" : ( warn ) => { console.log( warn ) },
+	"make_URI" : ( placeholder_array ){} //WIP
 	
 }
 
@@ -14,9 +16,17 @@ var PhotoMap = function( image ){
 }
 
 //Object Functions - these are the methods of PhotoMap
-//The plotting method is not needed, because when using the staging function inside PhotoMap instead of the generic staging function way down below, the need to plot is implied, so it is plotted right away, and the plot itself is returned instead of the source
+//The plotting method is used only as a getter method, because when using the staging function inside PhotoMap instead of the generic staging function way down below, the need to plot is implied, so it is plotted immediately once the image data is extracted
 
-PhotoMap.prototype.stage = function( source, method ){
+PhotoMap.prototype.stage = function( source, method, callback = function(){
+				
+				//default callback is called once image is loaded, and data is extracted.
+				
+				this.plot = new PhotoMap.PhotoMapPlot( this.source );
+				
+			}.bind( this/* - this 'this' is not the 'this' from the scope where this method was called, unless .call() was used to call the stage method and a this value was supplied*/ ) ){
+	
+	this.plot;
 	
 	switch ( 
 	
@@ -34,7 +44,7 @@ PhotoMap.prototype.stage = function( source, method ){
 			//break; - case -1 and 0 do the same thing
 		case 0: //error out - no original source, there is a new source, but no method
 		
-			loggerBox.error( 
+			placeHolders.error( 
 				"PhotoMap Object Staging ERROR: " +
 					"Original source does not exist, and the amount of arguments provided to the staging function is insufficient" +
 					"(" + ( !!source + 0 ) + " argument" + ( source ? "" : "s" ) + " given--function requires 2)"
@@ -47,30 +57,42 @@ PhotoMap.prototype.stage = function( source, method ){
 				//there is an original source, and the new source & method differ
 				
 			
-			this.source = PhotoMap.stage( method ? method: this.source.type, source );
+			this.source = PhotoMap.stage(
+			
+				method ? method: this.source.type,
+				source,
+				callback instanceof Function ? callback( this.source ) : undefined
+			
+			);
 				
 			break;
 		//case 2: //case 2 block can be deleted and is unnecessary as it will not do anything. All successful (no error) calls to this function will return this.source
 				
-				//return original source (via the plot object) - original source exists, but the new source & method differ or they are not provided in arguments (getter function)
+				//return original source - original source exists, but the new source & method differ or they are not provided in arguments (getter function)
 				
 			//break;
 		
 	}
 	
-	//read the note on why plotting is done immediately within staging using the comment above this function
+	return this.source;
 	
-	this.plot = new PhotoMap.PhotoMapPlot( this.source );
+}
+
+PhotoMap.prototype.plot = function(){
 	
-	return this.plot
-	
+	if( this.plot )
+		return this.plot;
+	else{
+		placeHolders.warn( "PhotoMap Object Plot WARNING: plot not yet defined, please wait!" );
+		return null;
+	}
 }
 
 PhotoMap.prototype.frame = function( plot ){
 	
 	if( !plot && !this.frame ){
 		
-		loggerBox.error( 
+		placeHolders.error( 
 			"PhotoMap Object Framing ERROR: " +
 				"Original plot either does not exist or has not yet been framed, and the amount of arguments provided to the framing function is insufficient" +
 				"(0 arguments given--function requires 1)"
@@ -90,7 +112,7 @@ PhotoMap.prototype.draw = function( frame ){
 	
 	if( !frame && !this.drawing ){
 		
-		loggerBox.error( 
+		placeHolders.error( 
 			"PhotoMap Object Framing ERROR: " +
 				"Original frame either does not exist or has not yet been drawn, and the amount of arguments provided to the drawing function is insufficient" +
 				"(0 arguments given--function requires 1)"
@@ -108,16 +130,16 @@ PhotoMap.prototype.draw = function( frame ){
 
 //Generic Functions - these are the non-method counterparts to each of the methods in PhotoMap
 
-PhotoMap.stage = ( method, source ) => {
+PhotoMap.stage = ( method, source, callback ) => {
 	
 	if( source === undefined ){
 		
-		loggerBox.error( "Photomap Generic Staging ERROR: what am I supposed to stage? I need a source!" );
+		placeHolders.error( "Photomap Generic Staging ERROR: what am I supposed to stage? I need a source!" );
 		return;
 		
 	}
 	
-	var modified = { "reference", "type" }
+	var modified = { "reference", "type", "data" }
 	
 	//either create image in blob or with filereader, or load from URL. The set the source reference
 	//if no arguments are supplied, error out
@@ -125,9 +147,10 @@ PhotoMap.stage = ( method, source ) => {
 	switch( method )
 	{
 		case undefined:
-			loggerBox.error( "PhotoMap Generic Staging ERROR: No arguments supplied" );
+			placeHolders.error( "PhotoMap Generic Staging ERROR: No arguments supplied" );
 			return;
-		case "URI": //Library Potential?
+		case "URI": //Library Potential? - use placeholder function
+			placeHolders.make_URI( [ source ] );
 			break;
 		case "Blob":
 			break;
@@ -135,8 +158,37 @@ PhotoMap.stage = ( method, source ) => {
 			break;
 		default:
 			return;
-		
 	}
+	
+	var img = new Image;
+	(( modifiee/*, type*/, callback ) => {
+		
+		//if revoking the source of the image is required, whether that be, because the image lives in the blob, or the DOM image object and/or URL is no longer needed uncomment this, if that depends on the source type as well
+		
+		img.addEventListener( "load", function (){
+		
+			//extract data using a virtual canvas:
+		
+			var virtual = document.createElement( "canvas" );
+			virtual.width = this.width;
+			virtual.height = this.height;
+		
+			var context = virtual.getContext( "2d" );
+		
+			context.drawImage( this, 0, 0 );
+			var modifee.data = context.getImageData( 0, 0, this.width , this.height );
+		
+			window.URL.revokeObjectURL( this.src );
+			this = null; //save resources.
+			
+			if( callback instanceof Function )
+				callback( modifiee );
+			
+		})
+		
+	})( modified/*, method*/, callback );
+	
+    img.src = modified.reference;
 	
 	modified.type = method;
 	
