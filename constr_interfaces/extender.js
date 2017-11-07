5f051865f3b50038e8fc94fb061d1d8563de6c37
smartguy1196 <smartguy1196@gmail.com>
2017-11-07 08:27:18 -0700

    Wiki-liized commentary
    - Comments have been removed, because of the wiki page addition

--- constr_interfaces/extender.js
+++ constr_interfaces/extender.js
@@ -1,45 +1,8 @@
 /*
 Commentary: pixiq Extender
 
-The library comes with a native extender function to allow developers to add to the API
-
-The extender use the global object defineProperty API to append to the pixiq prototype.
-
-Usage syntax:
-
-pixiq.extend( addition )
-
-Parameters:
-
-addition
-	- a list of extension definitions presented as an object (usually an object literal - easier to use)
-	- the addition object defines extensions with it's keys and values
-		- the keys are the names of the extensions to be added and later referenced by pixiq[name]
-		- the values in this object literal are descriptor objects (learn more at MDN - Object.defineProperty)
-			- the value key can be anything, but objects are the most useful
-			- *Note: the enumerable key can be set to false AND IS TRUE BY DEFAULT BY THIS LIBRARY
-				- if you don't want your library to be enumerable, set to false.
-
-Example:
-
-pixiq.extend({
-	
-	"cool_ext_bro" : {
-		
-		"enumerable" : true,
-		"value" : {
-			
-			"new_prop" : 1
-			"addon_method" : () => console.log( "hello world" );
-		
-		}
-		
-	}
-
-});
-pixiq.cool_ext_bro.new_prop //1
-pixiq[ "cool_ext_bro" ] //only works if enumerable is set to true
-pixiq[ "cool_ext_bro" ].addon_method() //console - hello world
+This script is well documented in the "Extensions and Extending" wiki. Questions should be directed there.
+Any other questions on pixiq extensions can be directed to StackOverflow or opened as an issue.
 */
 
 Object.defineProperties(
